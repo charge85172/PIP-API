0ab61ecd5c60c937c797bca54850dd3f9fa9b33a
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2026-06-02 14:41:24 +0200

    database withdummie data updated
    db.js  updated. we're using a existing db so  there is no use make a new table

--- db.js
+++ db.js
@@ -1,21 +1,10 @@
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('./pip_app.db', (err) => {
+const db = new sqlite3.Database('./Database/pip.sqlite', (err) => {
     if (err) {
         console.error('Database connection error:', err.message);
     } else {
         console.log('Connected to the PIP SQLite database.');
-
-        // Ensure the users table exists with a UNIQUE constraint on email
-        db.run(`
-            CREATE TABLE IF NOT EXISTS users (
-                                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                 name TEXT NOT NULL,
-                                                 email TEXT UNIQUE NOT NULL,
-                                                 password TEXT NOT NULL,
-                                                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-            )
-        `);
     }
 });
 
