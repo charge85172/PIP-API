f3b1b41a4145010d5591f35254fa3622510ef9ee
Ishika <esoebhag@gmail.com>
2026-06-04 15:10:20 +0200

    userRoutes fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,13 @@
-export default function userRoutes() {
+import express from 'express';
+import { getUserById } from '../controllers/authController.js';
+import { getUserProgress } from '../controllers/progressController.js';
 
-}
+const router = express.Router();
+
+// GET /api/users/:id
+router.get('/:id', getUserById);
+
+// GET /api/users/:id/progress
+router.get('/:id/progress', getUserProgress);
+
+export default router;
