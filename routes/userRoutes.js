4ecefb0366ba3cc8f2c14a03e9443dea99a94fa6
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2026-06-06 21:41:21 +0200

    user progress linked too lessons

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getUserById } from '../controllers/authController.js';
-import { getUserProgress } from '../controllers/userProgressionController.js'
+import { getUserProgress, completeLesson, openLesson } from '../controllers/userProgressionController.js'
 
 const router = express.Router();
 
@@ -9,5 +9,11 @@ router.get('/:id', getUserById);
 
 // GET /api/users/:id/progress
 router.get('/:id/progress', getUserProgress);
+
+// PUT /api/users/:id/progress/lesson/:lessonId
+router.put('/:id/progress/lesson/:lessonId', completeLesson);
+
+// PUT /api/users/:id/progress/lesson/:lessonId/open
+router.put('/:id/progress/lesson/:lessonId/open', openLesson);
 
 export default router;
