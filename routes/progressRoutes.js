17ab8c2a585fb89e6e777a27dc92c70275f55dfe
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2026-06-08 11:06:14 +0200

    switch save

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { startLessonAttempt, submitAttemptAnswer, completeLessonAttempt, getLessonAttempts } from '../controllers/progressController.js';
+import { startLessonAttempt, submitAttemptAnswer, completeLessonAttempt, getLessonAttempts, getLessonResult } from '../controllers/progressController.js';
 
 const router = express.Router();
 
 router.post('/lessons/:lessonId/start', startLessonAttempt);
 router.post('/attempts/:attemptId/answers', submitAttemptAnswer);
 router.post('/attempts/:attemptId/complete', completeLessonAttempt);
+
 router.get('/lessons/:lessonId/attempts/:userId', getLessonAttempts);
+router.get('/lessons/:lessonId/attempts/:attemptId/result', getLessonResult);
+
+
 export default router;
