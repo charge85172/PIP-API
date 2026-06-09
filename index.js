b2dd620584c352c4a0ccd811b46ff33dfe92a0f8
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2026-06-09 14:06:00 +0200

    Security headers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -14,6 +15,10 @@ dotenv.config();
 
 try {
     const app = express();
+
+    app.disable("x-powered-by");
+
+    app.use(helmet());
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
