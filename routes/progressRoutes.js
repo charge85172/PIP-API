6230fd335a8a0eb109f90017f69a6749bee5ccc4
Charge <Charge85172@gmail.com>
2026-06-16 13:03:07 +0200

    ik heb een route aangemaakt voor delete user progress van specific lessons, zodat je een user een les vanuit de backend opnieuw kunt laten doen, want tijdens het testen lukt het niet om xp te testen als je de lesson al eens gedaan had.

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -4,7 +4,8 @@ import {
     submitAttemptAnswer,
     completeLessonAttempt,
     getLessonResult,
-    getLessonAttempts
+    getLessonAttempts,
+    resetLessonCompletion,
 } from '../controllers/progressController.js';
 import {handlePostXP} from "../controllers/xpController.js";
 
@@ -15,11 +16,12 @@ router.post('/lessons/:lessonId/start', startLessonAttempt);
 router.post('/attempts/:attemptId/answers', submitAttemptAnswer);
 router.post('/attempts/:attemptId/complete', completeLessonAttempt);
 
-// XP Registration (New for User Story #25)
 // POST /api/progress/xp
 router.post('/xp', handlePostXP);
 router.get('/lessons/:lessonId/attempts/:attemptId', getLessonAttempts);
 router.get('/lessons/:lessonId/attempts/:attemptId/result', getLessonResult);
 
-
+// delete /api/progress/users/:userId/lessons/:lessonId/completion
+router.delete('/users/:userId/lessons/:lessonId/completion', resetLessonCompletion);
+//hiermee kun je lessen weer "open" zetten voor mensen, was ook nodig om xp in front end te testen.
 export default router;
